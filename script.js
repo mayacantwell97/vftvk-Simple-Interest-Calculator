72827deea207ab20580c8a482f43e098c04f66fd
mayacantwell97 <64390161+mayacantwell97@users.noreply.github.com>
2021-05-28 13:36:50 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function calculate()
-{
+{ //Get the values and calculate
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-if (principal < 1) {
+//Calculate the interest and text
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+//get for null value in Amount borrowed
+    if (principal < 1) {
     alert("Please enter the amount borrowed")};
 document.getElementById("result").style.backgroundColor = "yellow";
 }
-
+//move the slider value
 function updateRate()
 {
 var rateval = document.getElementById("rate").value;
