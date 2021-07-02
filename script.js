26e05edb30d383a1ff087810e24adcda9fdf4d28
mayacantwell97 <64390161+mayacantwell97@users.noreply.github.com>
2021-07-02 16:27:06 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
 //Calculate the interest and text
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-//get for null value in Amount borrowed
+    document.getElementById("result").innerHTML="If you deposit <div id="highlight" "+principal+",>\<br\>at an interest rate of <div id="highlight" "+rate+"% >\<br\>You will receive an amount of <div id ="highlight"+interest+">,\<br\>in the year <div id ="highlight"+"+year+"\<br\>"
+
+    //get for null value in Amount borrowed
     if (principal < 1) {
     alert("Please enter the amount borrowed");
         document.getElementById("principal").focus();
