fd7f1ab9729d299d048a28863478e304f345baa2
mayacantwell97 <64390161+mayacantwell97@users.noreply.github.com>
2021-07-03 14:53:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
 document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
  if (principal < 1) {
-    alert("Please enter the amount borrowed");
+    alert("Please enter a positive number in the amount borrowed");
         document.getElementById("principal").focus();
 
     };
